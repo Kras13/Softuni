604820edb870233bc149242cad594edf2aa29dd8
Kras13 <72404532+Kras13@users.noreply.github.com>
2022-06-03 17:21:45 +0300

    Add Extra tasks.

---
+++ CSharp Advanced/Extra/MultidimentionalArrays/MultidimentionalArrays/01.Diagonal Difference/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] diagonalsSum = new int[2];
+
+            for (int i = 0; i < size; i++)
+            {
+                int[] currentLine = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                diagonalsSum[0] += currentLine[i];
+                diagonalsSum[1] += currentLine[size - i - 1];
+            }
+
+            Console.WriteLine(Math.Abs(diagonalsSum[0] - diagonalsSum[1]));
+        }
+    }
+}
---
+++ CSharp Advanced/Extra/MultidimentionalArrays/MultidimentionalArrays/02.2X2 Squares in Matrix/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._2X2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int squareRowLimit = 2;
+            const int squareColLimit = 2;
+
+            int[] size = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(e => int.Parse(e))
+                .ToArray();
+
+            int row = size[0];
+            int col = size[1];
+
+            char[,] matrix = InitMatrix(row, col);
+            int result = NumberOfEqualSquares(matrix, squareRowLimit, squareColLimit);
+
+            Console.WriteLine(result);
+        }
+
+        private static int NumberOfEqualSquares(
+            char[,] matrix, int squareRowLimit, int squareColLimit)
+        {
+            int result = 0;
+
+            for (int row = 0; row <= matrix.GetLength(0) - squareRowLimit; row ++)
+            {
+                for (int col = 0; col <= matrix.GetLength(1) - squareColLimit; col ++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col] &&
+                        matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static char[,] InitMatrix(int row, int col)
+        {
+            char[,] result = new char[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                char[] currentLine = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(e => char.Parse(e))
+                    .ToArray();
+
+                for (int k = 0; k < col; k++)
+                {
+                    result[i, k] = currentLine[k];
+                }
+            }
+
+            return result;
+        }
+    }
+}
