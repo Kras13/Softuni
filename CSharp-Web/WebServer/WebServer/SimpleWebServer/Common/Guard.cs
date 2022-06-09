4d712ba430c0e737614b3c1078338c961c66315b
Kras13 <72404532+Kras13@users.noreply.github.com>
2022-06-09 10:52:08 +0300

    Add Guard.cs.

---
+++ CSharp-Web/WebServer/WebServer/SimpleWebServer/Common/Guard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleWebServer.Server.Common
+{
+    public static class Guard
+    {
+        public static void AgainstNull(object value, string name = null)
+        {
+            if (value == null)
+            {
+                if (name == null)
+                {
+                    name = "Value";
+                }
+
+                throw new InvalidOperationException($"{name} cannot be null!");
+            }
+        }
+    }
+}
