693bdc84e01cc01f646fe088b85f76c972119e75
Kras13 <paskalev.1989@abv.bg>
2022-06-26 16:27:05 +0300

    Add FIleLogger.cs

---
+++ CSharp-Web/WebServer/WebServer/WebServer/ConsoleAndFileLogger.cs
@@ -0,0 +1,17 @@
+﻿using SimpleWebServer.Tools;
+
+namespace SimpleWebServer.ConsoleApp
+{
+    public class ConsoleAndFileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LogLine(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CSharp-Web/WebServer/WebServer/WebServer/FIleLogger.cs
@@ -0,0 +1,25 @@
+﻿using SimpleWebServer.Tools;
+using System;
+using System.IO;
+
+namespace SimpleWebServer.ConsoleApp
+{
+    class FIleLogger : ILogger
+    {
+        private readonly FileStream fileStream;
+        public FIleLogger(string filePath)
+        {
+            this.fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+        }
+
+        public void Log(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogLine(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
