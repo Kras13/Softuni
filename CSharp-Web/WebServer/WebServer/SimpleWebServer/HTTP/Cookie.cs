2466e7e795b6e9124e41a573687f510b16acdf5f
Kras13 <paskalev.1989@abv.bg>
2022-06-29 01:23:41 +0300

    Add Cookie.cs + CookieCollection.cs.

---
+++ CSharp-Web/WebServer/WebServer/SimpleWebServer/HTTP/Cookie.cs
@@ -0,0 +1,25 @@
+﻿using SimpleWebServer.Server.Common;
+
+namespace SimpleWebServer.Server.HTTP
+{
+    public class Cookie
+    {
+        public Cookie(string name, string value)
+        {
+            Guard.AgainstNull(name, nameof(name));
+            Guard.AgainstNull(value, nameof(value));
+
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name}={this.Value}";
+        }
+    }
+}
---
+++ CSharp-Web/WebServer/WebServer/SimpleWebServer/HTTP/CookieCollection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SimpleWebServer.Server.HTTP
+{
+    public class CookieCollection : IEnumerable<Cookie>
+    {
+        private readonly Dictionary<string, Cookie> cookies;
+
+        public CookieCollection()
+        {
+            this.cookies = new Dictionary<string, Cookie>();
+        }
+
+        public string this[string name]
+        {
+            get
+            {
+                return this.cookies[name].Value;
+            }
+        }
+
+        public void Add(string name, string value)
+        {
+            this.cookies[name] = new Cookie(name, value);
+        }
+
+        public bool Contains(string name)
+        {
+            return this.cookies.ContainsKey(name);
+        }
+
+        public IEnumerator<Cookie> GetEnumerator()
+        {
+            return this.cookies.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
