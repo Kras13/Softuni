6a317c9f70aea477939b317bebf4e7b300d2a350
Kras13 <paskalev.1989@abv.bg>
2022-07-04 00:04:09 +0300

    Add Session.cs.

---
+++ CSharp-Web/WebServer/WebServer/SimpleWebServer/HTTP/Session.cs
@@ -0,0 +1,43 @@
+﻿using SimpleWebServer.Server.Common;
+using System.Collections.Generic;
+
+namespace SimpleWebServer.Server.HTTP
+{
+    public class Session
+    {
+        public const string SessionCookieName = "MyWebServerSID";
+
+        public const string SessionCurrentDateKey = "CurrentDate";
+
+        private Dictionary<string, string> data;
+
+        public Session(string id)
+        {
+            Guard.AgainstNull(id, nameof(id));
+
+            this.Id = id;
+
+            this.data = new Dictionary<string, string>();
+        }
+
+        public string Id { get; set; }
+
+        public string this[string key]
+        {
+            get
+            {
+                return this.data[key];
+            }
+
+            set
+            {
+                this.data[key] = value;
+            }
+        }
+
+        public bool Contains(string key)
+        {
+            return this.data.ContainsKey(key);
+        }
+    }
+}
