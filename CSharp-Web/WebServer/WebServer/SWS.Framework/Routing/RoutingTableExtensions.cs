80b7a0e900269dc294e908af4eb806cdfc30c4cc
Kras13 <72404532+Kras13@users.noreply.github.com>
2022-07-07 22:48:27 +0300

    Small changes.

--- CSharp-Web/WebServer/WebServer/SWS.Framework/Routing/RoutingTableExtensions.cs
+++ CSharp-Web/WebServer/WebServer/SWS.Framework/Routing/RoutingTableExtensions.cs
@@ -8,17 +8,63 @@ namespace SWS.Framework.Routing
 
     public static class RoutingTableExtensions
     {
-        public static IRoutingTable MapGet<TContoller>(
+        class PortClass
+        {
+            private  Response _response;
+            private Controller _controller;
+
+            private Func<Controller, Response> _controllerFunction;
+
+            protected PortClass(
+                Response response, 
+                Controller controller,
+                Func<Controller, Response> controllerFunction)
+            {
+                this._response = response;
+                this._controller = controller;
+                this._controllerFunction = controllerFunction;
+            }
+
+            public void InitController()
+            {
+
+            }
+
+            public Response MainInvokeFunc(Request request)
+            {
+
+                // TODO -> create controller with the request
+                // TODO -> call the givenfunction with the controller
+
+                return _response;
+            }
+
+            private TController CreateController<TController>(Request request)
+            {
+                return (TController)Activator.CreateInstance(typeof(TController), new[] { request });
+            }
+        }
+
+        public static IRoutingTable MapGet<TController>(
             this IRoutingTable routingTable,
             string path,
-            Func<TContoller, Response> controllerFunction) where TContoller : Controller
+            Func<TController, Response> controllerFunction) where TController : Controller
         {
             //return routingTable.MapGet(path, ActionFunction);
 
-
-
             return routingTable.MapGet(path, request => controllerFunction(
-                CreateController<TContoller>(request)));
+                CreateController<TController>(request)));
+        }
+
+        public static IRoutingTable MapGet2<TController>(
+            this IRoutingTable routingTable,
+            string path,
+            Func<Controller, Response> controllerFunction)
+        {
+            //return routingTable.MapGet(path, ActionFunction);
+
+            //return routingTable.MapGet(path, request => controllerFunction(
+            //    CreateController<TController>(request)));
         }
 
         public static IRoutingTable MapPost<TContoller>(
@@ -30,11 +76,10 @@ namespace SWS.Framework.Routing
                 CreateController<TContoller>(request)));
         }
 
+
         private static TController CreateController<TController>(Request request)
         {
             return (TController)Activator.CreateInstance(typeof(TController), new[] { request });
         }
     }
-
-    
 }
